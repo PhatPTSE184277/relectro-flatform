6cae718c6b01657b73b1316cc2f21e7c24ef6c31
Pham Tan Phat <tanphat120104@gmail.com>
2025-11-21 01:54:23 +0700

    fix menu

--- src/constants/colector/MenuItems.tsx
+++ src/constants/colector/MenuItems.tsx
@@ -12,9 +12,9 @@ import {
 
 export const collectorMenuItems = [
   { id: 'dashboard', label: 'Bảng điều khiển', path: '/small-collector/dashboard', icon: <LayoutDashboard size={20} /> },
-  { id: 'employee', label: 'Nhân viên', path: '/small-collector/employee', icon: <Users size={20} /> },
-  { id: 'shipping', label: 'Vận chuyển', path: '/small-collector/shipping', icon: <Truck size={20} /> },
-  { id: 'collection-point', label: 'Điểm thu gom', path: '/small-collector/collection-point', icon: <MapPin size={20} /> },
+  // { id: 'employee', label: 'Nhân viên', path: '/small-collector/employee', icon: <Users size={20} /> },
+  // { id: 'shipping', label: 'Vận chuyển', path: '/small-collector/shipping', icon: <Truck size={20} /> },
+  // { id: 'collection-point', label: 'Điểm thu gom', path: '/small-collector/collection-point', icon: <MapPin size={20} /> },
   { id: 'post', label: 'Bài đăng', path: '/small-collector/post', icon: <ClipboardList size={20} /> },
   { id: 'collection-route', label: 'Tuyến thu gom', path: '/small-collector/collection-route', icon: <Route size={20} /> },
   { id: 'incoming-warehouse', label: 'Nhận hàng về kho', path: '/small-collector/incoming-warehouse', icon: <Warehouse size={20} /> },
